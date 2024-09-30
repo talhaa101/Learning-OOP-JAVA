ed87ea2c3b677b58f2575f0ee2a745a938f745f5
Abu Talha <talha007abu@gmail.com>
2024-10-01 01:23:32 +0600

    ajker pora ses

--- OOP-TAW/day-01/Student.java
+++ OOP-TAW/day-01/Student.java
@@ -2,4 +2,19 @@ public class Student
 {
     public  String name ; // instance variable
     public  String id ; // instance variable
+
+    // public void m1(int n1 , String n2) // instance method
+    // {
+    //     System.out.println(n1 +" "+ n2 + " M1 is working");
+    // }
+
+    public void showDetail() // instance method
+    {
+        System.out.println(name + " " + id);
+    }
+
+    public void standUp() // instance method
+    {
+        System.out.println(name + " " + id + " is now standing");
+    }
 }
--- OOP-TAW/day-01/student_info_tester.java
+++ OOP-TAW/day-01/student_info_tester.java
@@ -1,6 +1,6 @@
 
 public class student_info_tester {
-    public static void main(String[] args) {
+    public static void main(String [] args) {
         int x = 5;
         Student s1 = new Student();
         Student s2 = new Student();
@@ -19,23 +19,31 @@ public class student_info_tester {
         // // System.out.println(arr2);
         // // System.out.println(arr1.length);
         // // System.out.println(arr2.length);
-
-        System.out.println(s1.name);
-        System.out.println(s1.id);
-    
-
-        System.out.println(s2.name);
-        System.out.println(s2.id);
-        System.out.println(" ");
-        s1 = s2;
+        //s1.m1(123 , "CSE");
         
-        System.out.println(s1.name);
-        System.out.println(s1.id);
-        System.out.println(s2.name);
-        System.out.println(s2.id);
+        // System.out.println(s1.name);
+        // System.out.println(s1.id);
+        // System.out.println(s2.name);
+        // System.out.println(s2.id);
+        // System.out.println(" ");
+        // s1 = s2;
+        // System.out.println(s1.name);
+        // System.out.println(s1.id);
+        // System.out.println(s2.name);
+        // System.out.println(s2.id);
+
+
         // System.out.println("\033c"); // anci code
         // System.out.println("\033[20;5H\033[38;5;240m Game Over! "); // anci code
 
+
+        // s1.showDetail();
+        // System.out.println("======================");
+        // s2.showDetail();
+
+        s1.standUp();
+        System.out.println("===============");
+        s2.standUp();
     }
 
 }
